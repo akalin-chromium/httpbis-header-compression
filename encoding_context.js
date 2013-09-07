811267b70ab607053a2a75965002aae4b5dce1a5
Frederick Akalin <akalin@gmail.com>
2013-09-06 17:49:23 -0700

    Have HeaderTable.try{Append,Replace}Entry() return an offset and index

--- encoding_context.js
+++ encoding_context.js
@@ -116,23 +116,30 @@ HeaderTable.prototype.removeFirstEntry_ = function() {
 }
 
 HeaderTable.prototype.tryAppendEntry = function(name, value) {
+  var index = -1;
+  var offset = 0;
   var sizeDelta = name.length + value.length + 32;
   while (this.entries_.length > 0 && this.size_ + sizeDelta > this.maxSize_) {
+    --offset;
     this.removeFirstEntry_();
   }
   if (this.size_ + sizeDelta <= this.maxSize_) {
     this.size_ += sizeDelta;
+    index = this.entries_.length;
     this.entries_.push({ name: name, value: value });
   }
+  return { offset: offset, index: index };
 }
 
 HeaderTable.prototype.tryReplaceEntry = function(index, name, value) {
+  var offset = 0;
   var existingEntry = this.entries_[index];
   var sizeDelta =
     (name.length + value.length + 32) -
     (existingEntry.name.length + existingEntry.value.length + 32);
   while (this.entries_.length > 0 && this.size_ + sizeDelta > this.maxSize_) {
     --index;
+    --offset;
     this.removeFirstEntry_();
   }
   if (this.size_ + sizeDelta <= this.maxSize_) {
@@ -141,9 +148,14 @@ HeaderTable.prototype.tryReplaceEntry = function(index, name, value) {
     if (index >= 0) {
       this.entries_[index] = newEntry;
     } else {
+      index = 0;
+      ++offset;
       this.entries_.unshift(newEntry);
     }
+  } else {
+    index = -1;
   }
+  return { offset: offset, index: index };
 }
 
 function ReferenceSet() {
