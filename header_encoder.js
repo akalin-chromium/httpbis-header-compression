bc0b244922e97be68ed67ea9c95adf8948c1ad4e
Frederick Akalin <akalin@gmail.com>
2013-09-10 14:30:35 -0700

    Move Encoder to header_encoder.js

--- header_encoder.js
+++ header_encoder.js
@@ -1,5 +1,114 @@
 'use strict';
 
+function Encoder() {
+  this.buffer_ = [];
+}
+
+Encoder.prototype.encodeOctet = function(o) {
+  this.buffer_.push(o & 0xff);
+};
+
+Encoder.prototype.encodeInteger = function(firstOctetMask, N, I) {
+  var nextMarker = (1 << N) - 1;
+
+  if (I < nextMarker) {
+    this.encodeOctet(firstOctetMask | I);
+    return;
+  }
+
+  if (N > 0) {
+    this.encodeOctet(firstOctetMask | nextMarker);
+  }
+
+  I -= nextMarker;
+  while (I >= 128) {
+    this.encodeOctet(I % 128 | 128);
+    I /= 128;
+  }
+  this.encodeOctet(I);
+}
+
+Encoder.prototype.encodeASCIIString = function(str) {
+  this.encodeInteger(0, 0, str.length);
+  for (var i = 0; i < str.length; ++i) {
+    this.encodeOctet(str.charCodeAt(i));
+  }
+}
+
+Encoder.prototype.encodeIndexedHeader = function(index) {
+  var opCode = 0x80;
+  var prefixLength = 7;
+  this.encodeInteger(opCode, prefixLength, index);
+}
+
+Encoder.prototype.encodeLiteralHeaderWithoutIndexing = function(
+  indexOrName, value) {
+  var opCode = 0x60;
+  var prefixLength = 5;
+  switch (typeof indexOrName) {
+    case 'number':
+      this.encodeInteger(opCode, prefixLength, indexOrName + 1);
+      this.encodeASCIIString(value);
+      return;
+
+    case 'string':
+      this.encodeInteger(opCode, prefixLength, 0);
+      this.encodeASCIIString(indexOrName);
+      this.encodeASCIIString(value);
+      return;
+  }
+
+  throw new Error('not an index or name: ' + indexOrName);
+}
+
+Encoder.prototype.encodeLiteralHeaderWithIncrementalIndexing = function(
+  indexOrName, value) {
+  var opCode = 0x20;
+  var prefixLength = 5;
+  switch (typeof indexOrName) {
+    case 'number':
+      this.encodeInteger(opCode, prefixLength, indexOrName + 1);
+      this.encodeASCIIString(value);
+      return;
+
+    case 'string':
+      this.encodeInteger(opCode, prefixLength, 0);
+      this.encodeASCIIString(indexOrName);
+      this.encodeASCIIString(value);
+      return;
+  }
+
+  throw new Error('not an index or name: ' + indexOrName);
+}
+
+Encoder.prototype.encodeLiteralHeaderWithSubstitutionIndexing = function(
+  indexOrName, substitutedIndex, value) {
+  var opCode = 0x0;
+  var prefixLength = 6;
+  switch (typeof indexOrName) {
+    case 'number':
+      this.encodeInteger(opCode, prefixLength, indexOrName + 1);
+      this.encodeInteger(0, 0, substitutedIndex);
+      this.encodeASCIIString(value);
+      return;
+
+    case 'string':
+      this.encodeInteger(opCode, prefixLength, 0);
+      this.encodeASCIIString(indexOrName);
+      this.encodeInteger(0, 0, substitutedIndex);
+      this.encodeASCIIString(value);
+      return;
+  }
+
+  throw new Error('not an index or name: ' + indexOrName);
+}
+
+Encoder.prototype.flush = function() {
+  var buffer = this.buffer_;
+  this.buffer_ = [];
+  return buffer;
+}
+
 function HeaderEncoder(direction) {
   this.encoder_ = new Encoder();
   this.encodingContext_ = new EncodingContext();
