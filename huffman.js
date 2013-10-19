318dc568a4c1b8c724d1af0dda9e690e2800ef62
U-bluebox\fenix <fenix@bluebox.(none)>
2013-10-19 15:51:09 -0700

    Fixed issues w.r.t. huffman encoding

--- huffman.js
+++ huffman.js
@@ -1,44 +1,53 @@
-
 'use strict';
 
 function getOptimalCodeLengths(weights) {
-  var nodes =
-    weights
+  var nodes = weights
     .map(function(w, i) { return { w: w, i: i }})
     .filter(function(n) { return n.w > 0; });
-  nodes.sort(function(a, b) {
+
+  function nodeCMP(a, b) {
     if (a.w < b.w) return -1;
     if (a.w > b.w) return +1;
+    if (a.i < b.i) return -1;
+    if (a.i > b.i) return +1;
     return 0;
-  });
+  }
+
+  nodes.sort(nodeCMP);
+
   var q1 = nodes;
   var q2 = [];
+
   function shiftMin() {
     if (q1.length == 0) return q2.shift();
     if (q2.length == 0) return q1.shift();
-    return (q1[0].w < q2[0].w) ? q1.shift() : q2.shift();
+    if (nodeCMP(q1[0], q2[0]) == -1) return q1.shift();
+    return q2.shift();
   }
+
   while (q1.length + q2.length > 1) {
     var n1 = shiftMin();
     var n2 = shiftMin();
     var n = { w: n1.w + n2.w, l: n1, r: n2 };
     q2.push(n);
   }
+
   var root = (q1.length > 0) ? q1[0] : q2[0];
   var codeLengths = [];
+
   function traverseDFS(n, d) {
     if (n.l) traverseDFS(n.l, d + 1);
     if (n.r) traverseDFS(n.r, d + 1);
     if ('i' in n) codeLengths[n.i] = d;
   }
-  traverseDFS(root, 1);
+
+  traverseDFS(root, 0);
   return codeLengths;
 }
 
 function codeLengthsToCanonicalEncoding(codeLengths) {
-  var nodes =
-    codeLengths
-    .map(function(l, i) { return { l: l, i: i }});
+  var nodes = codeLengths.map(function(l, i) { return { l: l, i: i }});
+
   nodes.sort(function(a, b) {
     if (a.l < b.l) return -1;
     if (a.l > b.l) return +1;
@@ -46,12 +55,14 @@ function codeLengthsToCanonicalEncoding(codeLengths) {
     if (a.i > b.i) return +1;
     return 0;
   });
+
   if (nodes.length > 0) {
     nodes[0].code = 0;
     for (var i = 1; i < nodes.length; ++i) {
       nodes[i].code = (nodes[i-1].code + 1) << (nodes[i].l - nodes[i - 1].l);
     }
   }
+
   var codes = [];
   for (var i = 0; i < nodes.length; ++i) {
     var j = nodes[i].i;
@@ -59,6 +70,40 @@ function codeLengthsToCanonicalEncoding(codeLengths) {
     codes[j] = nodes[i];
   }
   return codes;
+}
+
+function buildCodebook(freqTable) {
+  var divisor = 1;
+
+  while (true) {
+    var freqTableAdjusted = [];
+    for (var i = 0; i < freqTable.length; ++i) {
+      var freq = freqTable[i];
+      if (freq == 0) {
+        freq = 1.0 / 512.0;
+      }
+      freqTableAdjusted[i] = freq / divisor;
+    }
+    var codeLengths = getOptimalCodeLengths(freqTableAdjusted);
+    console.log(codeLengths);
+    var maxCodeLen = 0;
+    for (var i = 0; i < codeLengths.length; ++i) {
+      maxCodeLen = Math.max(maxCodeLen, codeLengths[i]);
+    }
+    if (maxCodeLen <= 32) break;
+    console.log(maxCodeLen)
+  }
+  return codeLengthsToCanonicalEncoding(codeLengths);
+}
+
+function buildInverseCodebook(codebook) {
+  var inverseCodebook = [];
+  for (var i = 0; i < codebook.length; ++i) {
+    if (codebook[i]) {
+      inverseCodebook[codebook[i].code] = { l: codebook[i].l, i: i };
+    }
+  }
+  return inverseCodebook;
 }
 
 function encodeBYTES(str, codebook) {
